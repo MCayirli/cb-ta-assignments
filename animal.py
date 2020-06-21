11412000133c95c72768990ecd780366135e4501
MCayirli <cayirli@gmail.com>
2020-06-21 16:03:20 +0300

    cat fonksiyonu eklendi

--- animal.py
+++ animal.py
@@ -1,10 +1,16 @@
 import sys                                                         
                                                                    
 def default():                                                     
-    print("hello")                                                 
-                                                                   
-def main():                                                        
-    default()                                                      
+    print("hello")                                                       
+
+def cat():                                                   
+    print("miav")                                            
+
+def main():                                                  
+    if sys.argv[1] == 'cat':                                  
+        cat()                                                
+    else:                                                    
+        default()                                                      
                                                                    
 if __name__ == "__main__":                                         
     main()
