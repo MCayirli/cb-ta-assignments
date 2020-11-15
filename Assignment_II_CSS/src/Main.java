214e133191d664a42555193f9b704a339c920012
MCayirli <cayirli@gmail.com>
2020-11-15 12:51:49 +0300

    2- Enter the key 'codingbook' to search field and press Enter

--- Assignment_II_CSS/src/Main.java
+++ Assignment_II_CSS/src/Main.java
@@ -6,11 +6,18 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Main {
 	// Assignment_II_CSS
 	public static void main(String[] args) throws InterruptedException {
-		
+
 		System.setProperty("webdriver.chrome.driver", "webdriver\\chromedriver.exe");
-		
+
 		// 1- Navigate to http://www.google.com (not https)
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://www.google.com");
+		Thread.sleep(100);
+
+		// 2- Enter the key "coding book" to search field and press Enter
+		WebElement q = driver.findElement(By.cssSelector("input[name = 'q']"));
+		q.sendKeys("codingbook");
+		Thread.sleep(100);
+		q.submit();
 	}
 }
